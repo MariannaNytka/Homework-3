63b50b21c1abaec2d846705df7cf4af152080c55
MariannaNytka <mariannanytka@gmail.com>
2024-04-20 13:33:17 +0200

    Task 1

--- Task 1.py
+++ Task 1.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    choosen_date= datetime.strptime(date, "%Y-%m-%d")
-    current_date=datetime.today()
-    delta = current_date - choosen_date
-    return delta.days
+    try:
+        choosen_date= datetime.strptime(date, "%Y-%m-%d")
+        current_date=datetime.today()
+        delta = current_date - choosen_date
+        return delta.days
+    except ValueError:
+        return ("Wrong data format. Use 'YYYY-MM-DD'")
+
 choosen_date= ("2024-05-20")
 print(get_days_from_today(choosen_date))
 
